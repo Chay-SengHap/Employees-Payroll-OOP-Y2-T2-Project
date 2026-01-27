119664bb21a6a9aaa4d787e71a09bb87807e6aec
Roth Sorayuth <sorayuthroth@gmail.com>
2026-01-27 14:26:48 +0700

    adding emp class

---
+++ Employee.java
@@ -0,0 +1,27 @@
+
+
+public class Employee {
+
+    String name;
+    int id;
+    String position;
+    Double baseSalary;
+    Double bonus;
+
+    public Employee(String name, int id, String position, Double baseSalary, Double bonus){
+        this.name = name;
+        this.id = id;
+        this.position = position;
+        this.baseSalary = baseSalary;
+        this.bonus = bonus;
+    }
+
+    void displayInfo(){
+        System.out.println("Name: "+ name);
+        System.out.println("ID: "+ id);
+        System.out.println("Position: "+ position);
+        System.out.println("Salary: "+ baseSalary);
+        System.out.println("Bonus: "+ bonus);
+       
+    }
+}
