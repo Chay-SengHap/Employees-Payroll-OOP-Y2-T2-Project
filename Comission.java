495be3a684cc0bc2d26c3914e057524d0b5b4118
Chay SengHap <senghabb@gmail.com>
2026-02-03 14:21:37 +0700

    change

--- Comission.java
+++ Comission.java
@@ -1,10 +1,10 @@
-class Commission {
+class Comission {
     String type;      // EXTRA_JOB, SIDE_VISIT, SALES
     double amount;
     
     int month;
     int year;
-    public Commission(String type, double amount, int month, int year) {
+    public Comission(String type, double amount, int month, int year) {
       this.type = type;
       this.amount = amount;
       this.month = month;
