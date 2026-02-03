a46377481d5fe9272a098e6f7b0c72f30848edfa
LySrongchhay <srongchhay99@gamil.com>
2026-02-03 14:11:22 +0700

    change

--- Employee.java
+++ Employee.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-
-
-public class Employee {
-
-    String name;
-    int id;
-    String position;
-    Double baseSalary;
-    Double bonus;
-    
-    public Employee(int id ,String name, String position, Double baseSalary, Double bonus){
-        this.name = name;
-        this.id = id;
-        this.position = position;
-        this.baseSalary = baseSalary;
-        this.bonus = bonus;
-    }
-
-    void displayInfo(){
-        System.out.println("ID: "+ id);
-        System.out.println("Name: "+ name);
-        System.out.println("Position: "+ position);
-        System.out.println("Salary: $"+ baseSalary);
-        System.out.println("Bonus: $"+ bonus);
-        System.out.println("Total: $"+ calculateSalary());
-
-       
-    }
-
-    double calculateSalary(){
-        return baseSalary+ bonus;
-    }
-=======
 
 public class Employee {
 
@@ -73,5 +39,4 @@ public class Employee {
 
 
     
->>>>>>> e1817e49ab28d3d358a09fd8991f676e23805ac3
 }
