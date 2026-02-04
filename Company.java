7c24fe8235337acfe91017d58eabd9d2a540c08c
Chay SengHap <senghabb@gmail.com>
2026-02-04 10:59:41 +0700

    initialize compamy class

--- Company.java
+++ Company.java
@@ -1,13 +1,32 @@
 public class Company {
     String companyName;
+    String industry;
     Employee[] employees;
     int employeeCount;
 
 
-    public Company(String companyName , String location , int maxEmployee){
+    public Company(String companyName , String industry){
         this.companyName = companyName;
+        this.industry = industry;
+        employees = new Employee[10];
+        employeeCount = 0 ;
        
     }
+    void addEmployee(Employee employee){
+        if (employeeCount == employees.length) {
+            Employee[] temp = new Employee[employees.length + 2];
+            for (int i = 0 ; i < employeeCount ; i++){
+                temp[i] = employees[i];
+            }
+            employees = temp;
+        }
+        employees[employeeCount++] = employee;
+    }
 
+    void displayAllEmployeePayrolls(){
+        for(int i = 0 ; i < employeeCount ; i++){
+            employees[i].display();
+        }
+    }
 
 }
--- Main.java
+++ Main.java
@@ -1,36 +1,33 @@
 public class Main{
 
     public static void main(String[] args){
-        
+        Company company = new Company("Spin the Wheel", "Chip Manufacture");
         Employee emp1 = new Employee("Yuth", 111);
         emp1.addPositionHistory(new PositionHistory("Staff", 100, "1/1/2026"));
-        // emp1.addPositionHistory(new PositionHistory("Manager", 1200, "1/2/2026"));
-        // emp1.addPayroll(new PayrollRecord(emp1, 02, 2026));
+        emp1.addPositionHistory(new PositionHistory("Manager", 1200, "1/2/2026"));
+        emp1.addPayroll(new PayrollRecord(emp1, 02, 2026));
 
-        // emp1.payrollsRecord[0].addCommision(new Comission("OutStanding", 150, 02, 2026));
-        // emp1.payrollsRecord[0].addCommision(new Comission("sava", 0.5, 02, 2026));
-        // emp1.addPayroll(new PayrollRecord(emp1, 03, 2026));
-        // emp1.payrollsRecord[1].addCommision(new Comission("Smos", 100, 03, 2026));
+        emp1.payrollsRecord[0].addCommision(new Comission("OutStanding", 150, 02, 2026));
+        emp1.payrollsRecord[0].addCommision(new Comission("sava", 0.5, 02, 2026));
+        emp1.addPayroll(new PayrollRecord(emp1, 03, 2026));
+        emp1.payrollsRecord[1].addCommision(new Comission("Smos", 100, 03, 2026));
         
-        // emp1.addPayroll(new PayrollRecord(emp1 , 04, 2026));
-        // emp1.payrollsRecord[2].addCommision(new Comission("Smos", 100, 03, 2026));
-        // emp1.payrollsRecord[2].addCommision(new Comission("Sad", 1500, 03, 2026));
-        // emp1.payrollsRecord[2].addCommision(new Comission("Smos", 100, 03, 2026));
-        // emp1.payrollsRecord[emp1.payrollCount - 1].addCommision(new Comission("Sad", 300, 03, 2026));
+        emp1.addPayroll(new PayrollRecord(emp1 , 04, 2026));
+        emp1.payrollsRecord[2].addCommision(new Comission("Smos", 100, 03, 2026));
+        emp1.payrollsRecord[2].addCommision(new Comission("Sad", 1500, 03, 2026));
+        emp1.payrollsRecord[2].addCommision(new Comission("Smos", 100, 03, 2026));
+        emp1.payrollsRecord[emp1.payrollCount - 1].addCommision(new Comission("Sad", 300, 03, 2026));
 
-        // emp1.addPayroll(new PayrollRecord(emp1, 10, 2002));
-        // emp1.addPositionHistory(new PositionHistory("Manager", 3000, "2/5/2029"));
-        // emp1.addPayroll(new PayrollRecord(emp1, 02,2030));
-        // emp1.payrollsRecord[emp1.payrollCount - 1].addCommision(new Comission("Big", 5000, 02, 2030));
+        emp1.addPayroll(new PayrollRecord(emp1, 10, 2002));
+        emp1.addPositionHistory(new PositionHistory("Manager", 3000, "2/5/2029"));
+        emp1.addPayroll(new PayrollRecord(emp1, 02,2030));
+        emp1.payrollsRecord[emp1.payrollCount - 1].addCommision(new Comission("Big", 5000, 02, 2030));
         
-        // emp1.addPositionHistory(new PositionHistory("Manager", 3000, "2/7/2029"));
+        emp1.addPositionHistory(new PositionHistory("Manager", 3000, "2/7/2029"));
         emp1.addPositionHistory(new PositionHistory("Manager", 3000, "2/9/2029"));
-        
 
-
-        
-        System.out.println(emp1.companyEmpId);
-        emp1.display();
+        company.addEmployee(emp1);
+        company.displayAllEmployeePayrolls();
        
 
 
